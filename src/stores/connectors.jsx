131817da4373d19916758dc17b5003b76781b85a
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-13 10:44:56 +0800

    update connectors

--- src/stores/connectors.jsx
+++ src/stores/connectors.jsx
@@ -21,11 +21,11 @@ export const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
-// export const network = new NetworkConnector({
-//   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
-//   defaultChainId: 1,
-//   pollingInterval: POLLING_INTERVAL
-// });
+export const network = new NetworkConnector({
+  urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
+  defaultChainId: 1,
+  pollingInterval: POLLING_INTERVAL
+});
 
 export const walletconnect = new WalletConnectConnector({
   rpc: { 1: RPC_URLS[1] },
@@ -36,7 +36,7 @@ export const walletconnect = new WalletConnectConnector({
 
 export const walletlink = new WalletLinkConnector({
   url: RPC_URLS[1],
-  appName: "ygov.finance"
+  appName: "yfii.finance"
 });
 
 export const ledger = new LedgerConnector({
@@ -56,7 +56,7 @@ export const trezor = new TrezorConnector({
 export const frame = new FrameConnector({ supportedChainIds: [1] });
 
 export const fortmatic = new FortmaticConnector({
-  apiKey: "pk_live_F95FEECB1BE324B5",
+  apiKey: "pk_live_BC11B495039A58C8",
   chainId: 1
 });
 
